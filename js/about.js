df18397c159aa581b98611c23bd49f09a24fc7f2
samalhaj1 <samalhaj627@gmail.com>
2021-07-11 15:33:29 +0300

    Add Literal Object TO About Us Page By Sam

---
+++ js/about.js
@@ -0,0 +1,143 @@
+'use strict';
+// alert("hi")
+
+let person1= {
+    name:"Jehad Muneer Abu Awwad",
+    currentYear:2021,
+    yearBorn:1997,
+    address:"Al Zouhor Neighborhood, Amman",
+    
+    likes:["Programming", "Traditional Dabke", "Sleeping"],
+    study: "Mechatronics Engineering",
+    age: function(yearBorn,currentYear){
+
+        return currentYear - yearBorn;
+       
+    }
+   
+
+}
+console.log('hi my name is:'
+
++ person1.name +',my address is:'
++ person1.address+ 'and i am ' 
++ person1.age(1997,2021)+',years old'
+);
+
+let person2= {
+    name:"Marwan Al-rawashdeh",
+    currentYear:2021,
+    yearBorn:1998,
+    address:"Jordan/Madaba.",
+    
+    likes:["sports" ,"watching movies "],
+    study: "civil engineering",
+    age: function(yearBorn,currentYear){
+
+        return currentYear - yearBorn;
+       
+    }
+   
+
+}
+console.log('hi my name is:'
+
++ person2.name +',my address is:'
++ person2.address+ 'and i am'
++ person2.age(1997,2021)+',years old'
+);
+
+let person3= {
+    name:"Malak Al-Khasawneh",
+    currentYear:2021,
+    yearBorn:1996,
+    address:"Jordan/Irbid",
+    
+    likes:["Reading novels" ,"Follow up on scientific program1"],
+    study: "pharmacy",
+    age: function(yearBorn,currentYear){
+
+        return currentYear - yearBorn;
+       
+    }
+   
+
+}
+console.log('hi my name is:'
+
++ person3.name +',my address is:'
++ person3.address+ 'and i am'
++ person3.age(1996,2021)+',years old'
+);
+
+
+let person4= {
+    name:"Faisal Kushha",
+    currentYear:2021,
+    yearBorn:1989,
+    address:"Jordan/amman.",
+    
+    likes:["swimming","writing"],
+    study: "programming",
+    age: function(yearBorn,currentYear){
+
+        return currentYear - yearBorn;
+       
+    }
+   
+
+}
+console.log('hi my name is:'
+
++ person4.name +',my address is:'
++ person4.address+ 'and i am'
++ person4.age(1989,2021)+',years old'
+);
+
+
+let person5= {
+    name:"Qais Alshorman",
+    currentYear:2021,
+    yearBorn:1989,
+    address:"Jordan/Irbid.",
+    
+    likes:["adventure","meditationq1"],
+    study: "programming",
+    age: function(yearBorn,currentYear){
+
+        return currentYear - yearBorn;
+       
+    }
+   
+
+}
+console.log('hi my name is:'
+
++ person5.name +',my address is:'
++ person5.address+ 'and i am'
++ person5.age(1989,2021)+',years old'
+);
+
+
+let peron6= {
+    name:"Sam Alhaj",
+    currentYear:2021,
+    yearBorn:1994,
+    address:"Jordan/Irbid.",
+    
+    likes:["swimming","writing","playing football"],
+    study: "programming, Translaion",
+    age: function(yearBorn,currentYear){
+
+        return currentYear - yearBorn;
+       
+    }
+   
+
+}
+console.log('hi my name is:'
+
++ peron6.name +',my address is:'
++ peron6.address+ 'and i am'
++ peron6.age(1994,2021)+',years old'
+);
