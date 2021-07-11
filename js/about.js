5e2ea30249c6f0486ac483948cda1b40cc13d643
jehadabuawwad <jehadabuawwad@outlook.com>
2021-07-11 22:28:34 +0300

    The About Us page of Rendering functionality Released by Jehad

--- js/about.js
+++ js/about.js
@@ -1,143 +1,123 @@
 'use strict';
-// alert("hi")
-
-let person1= {
-    name:"Jehad Muneer Abu Awwad",
-    currentYear:2021,
-    yearBorn:1997,
-    address:"Al Zouhor Neighborhood, Amman",
-    
-    likes:["Programming", "Traditional Dabke", "Sleeping"],
+/* This Part Made by Sam Al-Haj*/
+let person1 = {
+    name: "Jehad Abu Awwad",
+    currentYear: 2021,
+    yearBorn: 1997,
+    address: "Amman, Jordan",
+    likes: ["Programming", "Dabke"],
     study: "Mechatronics Engineering",
-    age: function(yearBorn,currentYear){
-
+    age: function(yearBorn, currentYear) {
         return currentYear - yearBorn;
-       
-    }
-   
+    },
+};
+
+
+let person2 = {
+    name: "Marwan Al-Rawashdeh",
+    currentYear: 2021,
+    yearBorn: 1998,
+    address: "Madaba, Jordan",
+    likes: ["Sports", "Watching Movies"],
+    study: "Civil Engineering",
+    age: function(yearBorn, currentYear) {
+        return currentYear - yearBorn;
+    },
+}
+
+let person3 = {
+    name: "Malak Al-Khasawneh",
+    currentYear: 2021,
+    yearBorn: 1996,
+    address: "Irbid,Jordan",
+    likes: ["Novels", "Scientific Programs"],
+    study: "Pharmacy",
+    age: function(yearBorn, currentYear) {
+        return currentYear - yearBorn;
+    },
+}
+
+
+
+let person4 = {
+    name: "Faisal Kushha",
+    currentYear: 2021,
+    yearBorn: 1989,
+    address: "Amman, Jordan",
+    likes: ["Swimming", "Writing"],
+    study: "Programming",
+    age: function(yearBorn, currentYear) {
+        return currentYear - yearBorn;
+    },
+}
+
+let person5 = {
+    name: "Qais Alshorman",
+    currentYear: 2021,
+    yearBorn: 1989,
+    address: "Irbid, Jordan",
+    likes: ["Adventure", "Meditation"],
+    study: "Programming",
+    age: function(yearBorn, currentYear) {
+        return currentYear - yearBorn;
+    },
 
 }
-console.log('hi my name is:'
 
-+ person1.name +',my address is:'
-+ person1.address+ 'and i am ' 
-+ person1.age(1997,2021)+',years old'
-);
-
-let person2= {
-    name:"Marwan Al-rawashdeh",
-    currentYear:2021,
-    yearBorn:1998,
-    address:"Jordan/Madaba.",
-    
-    likes:["sports" ,"watching movies "],
-    study: "civil engineering",
-    age: function(yearBorn,currentYear){
-
-        return currentYear - yearBorn;
-       
-    }
-   
-
-}
-console.log('hi my name is:'
-
-+ person2.name +',my address is:'
-+ person2.address+ 'and i am'
-+ person2.age(1997,2021)+',years old'
-);
-
-let person3= {
-    name:"Malak Al-Khasawneh",
-    currentYear:2021,
-    yearBorn:1996,
-    address:"Jordan/Irbid",
-    
-    likes:["Reading novels" ,"Follow up on scientific program1"],
-    study: "pharmacy",
-    age: function(yearBorn,currentYear){
-
-        return currentYear - yearBorn;
-       
-    }
-   
-
-}
-console.log('hi my name is:'
-
-+ person3.name +',my address is:'
-+ person3.address+ 'and i am'
-+ person3.age(1996,2021)+',years old'
-);
-
-
-let person4= {
-    name:"Faisal Kushha",
-    currentYear:2021,
-    yearBorn:1989,
-    address:"Jordan/amman.",
-    
-    likes:["swimming","writing"],
-    study: "programming",
-    age: function(yearBorn,currentYear){
-
-        return currentYear - yearBorn;
-       
-    }
-   
-
-}
-console.log('hi my name is:'
-
-+ person4.name +',my address is:'
-+ person4.address+ 'and i am'
-+ person4.age(1989,2021)+',years old'
-);
-
-
-let person5= {
-    name:"Qais Alshorman",
-    currentYear:2021,
-    yearBorn:1989,
-    address:"Jordan/Irbid.",
-    
-    likes:["adventure","meditationq1"],
-    study: "programming",
-    age: function(yearBorn,currentYear){
-
-        return currentYear - yearBorn;
-       
-    }
-   
-
-}
-console.log('hi my name is:'
-
-+ person5.name +',my address is:'
-+ person5.address+ 'and i am'
-+ person5.age(1989,2021)+',years old'
-);
-
-
-let peron6= {
-    name:"Sam Alhaj",
-    currentYear:2021,
-    yearBorn:1994,
-    address:"Jordan/Irbid.",
-    
-    likes:["swimming","writing","playing football"],
+let person6 = {
+    name: "Sam Alhaj",
+    currentYear: 2021,
+    yearBorn: 1994,
+    address: "Irbid, Jordan",
+    likes: ["Swimming", "Writing", "Football"],
     study: "programming, Translaion",
-    age: function(yearBorn,currentYear){
-
+    age: function(yearBorn, currentYear) {
         return currentYear - yearBorn;
-       
-    }
-   
+    },
 
 }
-console.log('hi my name is:'
 
-+ peron6.name +',my address is:'
-+ peron6.address+ 'and i am'
-+ peron6.age(1994,2021)+',years old'
-);
+/* This Part Made by Jehad Abu Awwad*/
+let arrayOfPersons = [person1, person2, person3, person4, person5, person6]
+let arrayOfInfo = ["jehadInfo", "marwanInfo", "malakInfo", "faisalInfo", "qaisInfo", "samInfo"]
+
+for (let j = 0; j < arrayOfInfo.length; j++) {
+    let ul = document.getElementById(`${arrayOfInfo[j]}`);
+    let list = document.createElement('li');
+    ul.appendChild(list);
+    list.textContent = `Name :${arrayOfPersons[j].name}`;
+}
+
+for (let j = 0; j < arrayOfInfo.length; j++) {
+    let ul = document.getElementById(`${arrayOfInfo[j]}`);
+    let list = document.createElement('li');
+    ul.appendChild(list);
+    list.textContent = `Age :${arrayOfPersons[j].age(arrayOfPersons[j].yearBorn, arrayOfPersons[j].currentYear)}`;
+}
+
+for (let j = 0; j < arrayOfInfo.length; j++) {
+    let ul = document.getElementById(`${arrayOfInfo[j]}`);
+    let list = document.createElement('li');
+    ul.appendChild(list);
+    list.textContent = `Address :${arrayOfPersons[j].address}`;
+}
+
+for (let j = 0; j < arrayOfInfo.length; j++) {
+    let ul = document.getElementById(`${arrayOfInfo[j]}`);
+    let list = document.createElement('li');
+    ul.appendChild(list);
+    list.textContent = `Interests :${arrayOfPersons[j].likes}`;
+}
+
+for (let j = 0; j < arrayOfInfo.length; j++) {
+    let ul = document.getElementById(`${arrayOfInfo[j]}`);
+    let list = document.createElement('li');
+    ul.appendChild(list);
+    list.textContent = `Study :${arrayOfPersons[j].study}`;
+}
+
+
+var card = document.querySelector('.card');
+card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped');
+});
