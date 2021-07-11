def9c67be0479a5a2cfcfb2241ec95b8b6fcbb0e
Faisal-Kushha <fkushha89@hotmail.com>
2021-07-11 22:08:41 +0300

    Render done by Faisal and Qais

--- js/Adventure.js
+++ js/Adventure.js
@@ -1,95 +1,91 @@
-'use strict';
-
+"use strict";
 
 Tour.adventure = [];
 
-function Tour(name, email, phone, packages, number, date, vaccine){
-    this.name = name;
-    this.email = email;
-    this.phone = phone;
-    this.packages = packages;
-    this.number = number;
-    this.date = date;
-    this.vaccine = vaccine;
-    Tour.adventure.push(this);
-    saveData();
-
+function Tour(name, email, phone, packages, number, date, vaccine) {
+  this.name = name;
+  this.email = email;
+  this.phone = phone;
+  this.packages = packages;
+  this.number = number;
+  this.date = date;
+  this.vaccine = vaccine;
+  Tour.adventure.push(this);
+  saveData();
 }
 
 
+const UserInput = document.getElementById("UserInput");
+UserInput.addEventListener("submit", handleSubmit);
 
-const UserInput = document.getElementById('UserInput');
-UserInput.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
 
-function handleSubmit(event){
-    event.preventDefault();
+  let newName = event.target.nameField.value;
+  let newEmail = event.target.emailField.value;
+  let newPhone = event.target.phoneField.value;
+  let newPackages = event.target.package.value;
+  let newNumber = event.target.touristField.value;
+  let newDate = event.target.dateField.value;
+  let newVaccine = event.target.vaccine.value;
 
-    let newName = event.target.nameField.value;
-    let newEmail = event.target.emailField.value;
-    let newPhone = event.target.phoneField.value;
-    let newPackages = event.target.package.value;
-    let newNumber = event.target.touristField.value;
-    let newDate = event.target.dateField.value;
-    let newVaccine = event.target.vaccine.value;
-
-    new Tour(newName, newEmail, newPhone, newPackages, newNumber, newDate, newVaccine);
-
-    // console.log(Tour.adventure)
+  new Tour(newName, newEmail, newPhone, newPackages, newNumber, newDate, newVaccine);
+  renderTicket();
 }
 
-function saveData(){
-    let x = JSON.stringify(Tour.adventure);
-    localStorage.setItem("User", x)
+function saveData() {
+  let x = JSON.stringify(Tour.adventure);
+  localStorage.setItem("User", x);
 }
 
+function loadData() {
+  let y = localStorage.getItem("User");
+  let parseData = JSON.parse(y);
+  if (parseData) {
+    Tour.adventure = parseData;
+    
+  }
+}
 
-function loadData(){
-    let y = localStorage.getItem ("User");
-    let parseData = JSON.parse(y);
-        if (parseData) {
-            // Tour.adventure = parseData;
-            for(let i = 0 ; i < parseData.length; i++){
-                let reInst = new Tour(parseData[i].name, parseData[i].email, parseData[i].phone, parseData[i].packages, parseData[i].number, parseData[i].date, parseData[i].vaccine)
-                console.log(reInst);
-            }
+loadData();
 
+
+const Ticket = document.getElementById("ticketa");
+
+function renderTicket() {
+     Ticket.textContent = '';
+        
+        let lastIndexInArray = Tour.adventure.length-1;
+        let ticket1 = document.createElement("div");
+        Ticket.appendChild(ticket1);
+        let par1 = document.createElement("pre");
+        ticket1.appendChild(par1);
+        par1.textContent = `Full Name: ${Tour.adventure[lastIndexInArray].name}                        E-mail: ${Tour.adventure[lastIndexInArray].email}                        PhoneNumber: ${Tour.adventure[lastIndexInArray].phone} `;
+        let par2 = document.createElement("pre");
+        ticket1.appendChild(par2);
+        par2.textContent = `Desired Package: ${Tour.adventure[lastIndexInArray].packages}              Number of Tourists: ${Tour.adventure[lastIndexInArray].number}                                Tour Date: ${Tour.adventure[lastIndexInArray].date} `;
+        let par3 = document.createElement("pre");
+        ticket1.appendChild(par3);
+        par3.textContent = `Vaccinated Against Covid-19: ${Tour.adventure[lastIndexInArray].vaccine} `;
+        let par4 = document.createElement("pre");
+        ticket1.appendChild(par4);
+        par4.textContent = `BASED TO THE JORDANIAN GOVERNMENT REGULATION REGARDING TO COVID-19 YOU HAVE TO BE EITHER VACCINATED OR PROVIDE A PCR TEST IN THE LAST 72 HOURS `;
+        let par6 = document.createElement("pre");
+        ticket1.appendChild(par6);
+        if (Tour.adventure[lastIndexInArray].packages ===  "3 Days & 2 Nights") {
+            let total1 = Math.floor(Tour.adventure[lastIndexInArray].number * 150);
+            par6.textContent = `Total Cost of Your Tour: ` + total1 + ' JOD';
+        } else if (Tour.adventure[lastIndexInArray].packages === "5 Days & 4 Nights") {
+            let total2 = Math.floor(Tour.adventure[lastIndexInArray].number * 250);
+            par6.textContent = `Total Cost of Your Tour: ` + total2 + ' JOD';
+        } else
+        {   let total3 = Math.floor(Tour.adventure[lastIndexInArray].number * 350);
+            par6.textContent = `Total Cost of Your Tour: ` + total3 + ' JOD';
         }
+  
+    if (Tour.adventure.length >= 1) {
+        UserInput.removeEventListener("submit", handleSubmit);
+        UserInput.reset();
     }
-    loadData();
-    // UserInput.removeEventListener('submit', handleSubmit);
-    // UserInput.reset();
+}
 
-
-    // Tour.prototype.adventure = function(){
-//     let package1 = ("3 Days & 2 Nights");
-//     let package2 = ("5 Days & 4 Nights");
-//     let package3 = ("7 Days & 6 Nights");
-//         if (this.packages === package1) {
-//             return Math.floor(this.number * 150);
-        
-//         } 
-//         else if (this.packages === package2) 
-//         {
-//             return Math.floor(this.number * 250);
-        
-//         } 
-//         else (this.packages === package3)
-//          {
-//             return Math.floor(this.number * 350);
-//         }
-
-// }
-
-
-// function vac(){
-//     let vaccine1 = this. vaccine ;
-//     switch (vaccine1){
-//         case 'Yes':
-//             render();
-//             break;
-//             case 'No':
-//                 textcontet
-//                 'Based on the Jordanian government regulation regarding to Covid-19 you have to be either vaccinated or provide a PCR test in last 72 hours you can check: https://international.visitjordan.com/page/31/Covid-19Update'
-//                 break;
-//             }
-// // }
